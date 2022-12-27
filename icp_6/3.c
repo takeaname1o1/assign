d903b31c77861f64a8c09e3f1c4cac2612cafdc5
ALPSTONE <jitenkr129152@Gmail.com>
2022-12-27 23:01:31 +0530

    d

--- icp_6/3.c
+++ icp_6/3.c
@@ -1,31 +1,12 @@
 #include <stdio.h>
-#include <math.h>
-#include "random.h"
-
-#define N 10  // Number of elements in the arrays
-
+// delete binary file
 int main(void) {
-  int i;
-  double X[2][N], Y[2][N], sum = 0.0;
-
-  // Initialize the arrays with random numbers
-  for (i = 0; i < N; i++) {
-    X[0][i] = random32();
-    X[1][i] = random32();
-    Y[0][i] = random32();
-    Y[1][i] = random32();
+  // Delete the file "test.bin"
+  if (remove("3") == 0) {
+    printf("File deleted successfully.\n");
+  } else {
+    printf("Error deleting file.\n");
   }
-
-  // Calculate the sum of the terms in the equation
-  for (i = 0; i < N; i++) {
-    sum += (X[1][i] - X[0][i]) * (X[1][i] - X[0][i]) - (Y[1][i] - Y[0][i]) * (Y[1][i] - Y[0][i]);
-  }
-
-  // Calculate the result of the equation
-  double result = sum / (N - 1);
-
-  // Print the result
-  printf("Result: %f\n", result);
 
   return 0;
 }
